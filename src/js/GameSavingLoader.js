862cb4ab2413beaf5a70f20cf46f7f0a4d6ba442
Anna Kuznetsova <ann_kuzn@mail.ru>
2024-10-02 21:20:52 +0500

    first commit

---
+++ src/js/GameSavingLoader.js
@@ -0,0 +1,14 @@
+import read from "./reader";
+import json from "./parser";
+
+export default class GameSavingLoader {
+  async load() {
+    try {
+      let data = await read();
+      let value = await json(data);
+      return JSON.parse(value);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+}
